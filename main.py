6d5aa59807f12486e86bda4383fc004285b42b7b
glaremage <jrfabella@gmail.com>
2025-05-03 21:53:12 -0400

    added rotation to player

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         pygame.Surface.fill(screen, (0,0,0))
         player.draw(screen)
         pygame.display.flip()
